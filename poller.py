309e9c8070d4af21679b296b9571add451ba209a
kali <kali@kali.tdzfgsdi>
2022-05-24 22:37:05 +0200

    Cleanup and function to extract to dict

--- poller.py
+++ poller.py
@@ -1,5 +1,7 @@
 from miflora.miflora_poller import MiFloraPoller
 from btlewrap.bluepy import BluepyBackend
+from btlewrap.base import BluetoothBackendException
+from miflora.miflora_poller import MiFloraPoller, MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
 import os, yaml
 
 import sys
@@ -8,19 +10,27 @@ def load_config(file):
     with open(file, 'r') as f:
         return yaml.safe_load(f)
 
+def get_miflora_data_dict(poller):
+    d = dict()
+    try:
+        d['firmware'] = poller.firmware_version()
+        d['name'] = poller.name()
+        d['temperature'] = poller.parameter_value(MI_TEMPERATURE)
+        d['moisture'] = poller.parameter_value(MI_MOISTURE)
+        d['light'] = poller.parameter_value(MI_LIGHT)
+        d['conductivity'] = poller.parameter_value(MI_CONDUCTIVITY)
+        d['battery'] = poller.parameter_value(MI_BATTERY)
+    except BluetoothBackendException as e:
+        d['data'] = 'no data'
+    return d
 
 
-configuration = load_config("{}/{}".format("/home/pi", "floral-config.yaml"))
-sensors = configuration.get("sensors")
-php_param = sensors[1].get('bluetooth_mac_address')
-poller = MiFloraPoller(php_param, BluepyBackend)
 
-from miflora.miflora_poller import MiFloraPoller, MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
+if __name__ == "__main__":
+    configuration = load_config("{}/{}".format("/home/pi", "floral-config.yaml"))
 
-print("\"{}\"".format(poller.firmware_version()))
-print("\"{}\"".format(poller.name()))
-print("\"{}\"".format(poller.parameter_value(MI_TEMPERATURE)))
-print("\"{}\"".format(poller.parameter_value(MI_MOISTURE)))
-print("\"{}\"".format(poller.parameter_value(MI_LIGHT)))
-print("\"{}\"".format(poller.parameter_value(MI_CONDUCTIVITY)))
-print("\"{}\"".format(poller.parameter_value(MI_BATTERY)))
+    sensors = configuration.get("sensors")
+    bt_mac = sensors[1].get('bluetooth_mac_address')
+    poller = MiFloraPoller(bt_mac, BluepyBackend)
+    miflora_data_dict = get_miflora_data_dict(poller)
+    print(miflora_data_dict['moisture'])
