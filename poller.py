5200a5c006fc9620c68af9ca521699405435265e
kali <kali@kali.tdzfgsdi>
2022-05-25 00:58:13 +0200

    Only run once for now

--- poller.py
+++ poller.py
@@ -38,17 +38,16 @@ if __name__ == "__main__":
     env = Environment( loader = FileSystemLoader(templates_dir) )
     template = env.get_template('index.html')
 
-    for _ in range(4):
-        for sensor in sensors:
-            bluetooth_mac_adress = sensor.get('bluetooth_mac_address')
-            sensor_name = sensor.get('name')
-            plant = sensor.get('plant')
-            print("Trying to connect to " + str(sensor_name) + " with MAC " +str(bluetooth_mac_adress) + " on plant " + plant)
-            poller = MiFloraPoller(bluetooth_mac_adress, BluepyBackend)
-            miflora_data_dict = get_miflora_data_dict(plant, poller)
-            if 'firmware' in miflora_data_dict:
-                sensory_data_array.append(miflora_data_dict)
-                print(str(sensor_name) + " meldet eine Feuchtigkeit von " + str(miflora_data_dict['moisture']) +u"% sowie einen Nährstoffgehalt von " + str(miflora_data_dict['conductivity']) + "uS/cm")
+    for sensor in sensors:
+        bluetooth_mac_adress = sensor.get('bluetooth_mac_address')
+        sensor_name = sensor.get('name')
+        plant = sensor.get('plant')
+        print("Trying to connect to " + str(sensor_name) + " with MAC " +str(bluetooth_mac_adress) + " on plant " + plant)
+        poller = MiFloraPoller(bluetooth_mac_adress, BluepyBackend)
+        miflora_data_dict = get_miflora_data_dict(plant, poller)
+        if 'firmware' in miflora_data_dict:
+            sensory_data_array.append(miflora_data_dict)
+            print(str(sensor_name) + " meldet eine Feuchtigkeit von " + str(miflora_data_dict['moisture']) +u"% sowie einen Nährstoffgehalt von " + str(miflora_data_dict['conductivity']) + "uS/cm")
     
     filename = os.path.join(root, 'html', 'index.html')
     with open(filename, 'w') as fh:
