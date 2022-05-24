c283c7c04f7bb78b247d8510af3b1c3c661fabf8
kali <kali@kali.tdzfgsdi>
2022-05-24 22:10:25 +0200

    Grundlegende Funktionalität zum laden einer configuration

--- poller.py
+++ poller.py
@@ -1,8 +1,16 @@
 from miflora.miflora_poller import MiFloraPoller
 from btlewrap.bluepy import BluepyBackend
+import os, yaml
 
 import sys
 
+def load_config(file):
+    with open(file) as f:
+        return yaml.load(f)
+
+
+
+configuration = load_yaml("{}/{}".format("/home/pi", "floral-config.yaml"))
 php_param = sys.argv[1]
 poller = MiFloraPoller(php_param, BluepyBackend)
 
